c7e6ecde8bb11e6b7eab71414a1ccb08ff4cc36d
佐藤舞奈 <maina1216@icloud.com>
2026-04-04 12:46:01 +0000

    ログイン画面を編集

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,11 +16,14 @@
     @endif
     <form action="/admin/login" method="post">
         @csrf
-        ログインID : <input type="text" name="login_id" value="{{ old('login_id') }}"><br>
+        ログインID : <input type="text" name="login_id" value="{{ old('login_id','hogemin') }}"><br>
         パスワード : <input type="password" name="password"><br>
+        (※デモ用パスワード：pass)<br>
         <button>登録する</button>
     </form>
     <br>
     <a href='/admin/register'>管理者登録</a>
+    <br><br>
+    <a href='/reservation/list'>各院予約サイト</a>
 </div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,11 +16,14 @@
     @endif
     <form action="/login" method="post">
         @csrf
-        email : <input type="email" name="email" value="{{ old('emaik') }}"><br>
+        email : <input type="email" name="email" value="{{ old('email' ,'nara@example.com') }}"><br>
         パスワード : <input type="password" name="password"><br>
+        (※デモ用パスワード：nara)<br>
         <button>ログインする</button>
     </form>
     <br>
     <a href="/user/register">新規院登録</a>
+    <br><br>
+    <a href='/admin'>管理者用サイト</a>
 </div>
 @endsection
